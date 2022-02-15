1ee1ba71735b77c60ab565d26dd9391e45f4ae0e
tina_m <librariantinamorrell@gmail.com>
2022-02-14 20:38:55 -0700

    elif and lese statement project

---
+++ birthday-reminder-app.py
@@ -0,0 +1,10 @@
+#birthday reminder app - elif and else statement usage project example
+user_input = input("Enter a person's name: ")
+if user_input == "Lynn":
+  print("Birthday is 4/15/70")
+elif user_input == "David":
+  print("Birthday is 10/3/73")
+elif user_input == "Tammy":
+  print("Birthday is 6/12/65")
+else:
+  print("Cant find birthday for this person...")
